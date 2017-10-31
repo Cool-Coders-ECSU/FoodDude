10c4c6603afc2bfe97d3bdce718a077670009388
drcouncil <32073982+drcouncil@users.noreply.github.com>
2017-10-30 20:29:18 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,99 +1,183 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package fooddude;
 
-import java.util.Scanner;
-import java.text.DecimalFormat;
-/**
- *
- * @author drcouncil
+  import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+ 
+
+ 
+/*
+ * CustomComboBoxDemo.java uses the following files:
+ *   images/Bird.gif
+ *   images/Cat.gif
+ *   images/Dog.gif
+ *   images/Rabbit.gif
+ *   images/Pig.gif
  */
-
-
-
-public class Main {
-    
-    private static DecimalFormat df2 = new DecimalFormat(".00");
-    
-    public static void main(String args[])
-    {
-        double price=0;// Saved amount for customer
-        double tax =0.05; 
-        char choice = 'y';// decision from customer
-     String breakfast = "BREAKFAST\0";
-        Menu t = new Menu();
-        //Prompt user for which type of meal
-    System.out.println("Hello, thanks for coming to eat at FoodDude choose from Dinner, Dessert, and Breakfast:");
-     
-    Scanner scan = new Scanner(System.in);
-    String input = scan.nextLine();
-  
-    
-    
-    System.out.print("Your input - " + input.toUpperCase() +"\n" );
-    
-        
-   // do{
-    //PASS STRING TO MENU CLASS AND DISPLAY MENU
-    if (input.toUpperCase() == breakfast)
-    {
-    System.out.println("\n\tBREAKFAST MENU\n ------------------------------------");
-        t.BreakfastMenu();
-        System.out.println("\nChoose from the list above which number you will like: \n (choose 0 to exit)");
-        
+public class Main extends JPanel {
+    ImageIcon[] images;
+    String[] petStrings = {"Bird", "Cat", "Dog", "Rabbit", "Pig"};
+ 
+    /*
+     * Despite its use of EmptyBorder, this panel makes a fine content
+     * pane because the empty border just increases the panel's size
+     * and is "painted" on top of the panel's normal background.  In
+     * other words, the JPanel fills its entire background if it's
+     * opaque (which it is by default); adding a border doesn't change
+     * that.
+     */
+    public Main() {
+        super(new BorderLayout());
+ 
+        //Load the pet images and create an array of indexes.
+        images = new ImageIcon[petStrings.length];
+        Integer[] intArray = new Integer[petStrings.length];
+        Integer[] intArray2 = new Integer[petStrings.length];
+        for (int i = 0; i < petStrings.length; i++) {
+            intArray[i] = new Integer(i);
+            images[i] = createImageIcon("images/" + petStrings[i] + ".gif");
+            if (images[i] != null) {
+                images[i].setDescription(petStrings[i]);
+            }
         }
-       //---------------------------------------------------------------------------------------------- 
-    
-    
-        else if(input.toUpperCase() == "DINNER")
-        {
-            System.out.println("\n\tDINNER MENU\n ----------------------------------");
-        t.DinnerMenu();
-        System.out.println("\nChoose from the list above which number you will like: \n (Choose 0 to exit)");
+        
+         for (int i = 0; i < petStrings.length; i++) {
+            intArray2[i] = new Integer(i);
+            images[i] = createImageIcon("images/" + petStrings[i] + ".gif");
+            if (images[i] != null) {
+                images[i].setDescription(petStrings[i]);
+            }
         }
-       //------------------------------------------------------------------------------------------------
+        
+ 
+        //Create the combo box.
+        JComboBox petList = new JComboBox(intArray);
+        ComboBoxRenderer renderer= new ComboBoxRenderer();
+        renderer.setPreferredSize(new Dimension(200, 130));
+        petList.setRenderer(renderer);
+        petList.setMaximumRowCount(8);
         
         
-        else if(input != "DESSERT")
-        {
-    System.out.println("\n\tDESSERT MENU\n ------------------------------------");
-        t.DessertMenu();
+        JComboBox items = new JComboBox(intArray2);
+        ComboBoxRenderer renderer1= new ComboBoxRenderer();
+        renderer1.setPreferredSize(new Dimension(200, 130));
+        items.setRenderer(renderer1);
+        items.setMaximumRowCount(8);
         
-        double n;
-        System.out.println("\nChoose from the list above which number you will like: \n (choose 0 to exit)");
-          n = scan.nextDouble();
-          //System.out.println("You enter " + n + " --");
+        //Lay out the demo.
+        add(petList, BorderLayout.PAGE_START);
+        setBorder(BorderFactory.createEmptyBorder(10,10,20,20));
         
         
-        System.out.println("Your total is $"+df2.format(t.Dessert(n)));
-       price += t.Dessert(n);
-       
-        }
-      //-----------------------------------------------------------------------------------------------------------
+         add(items, BorderLayout.EAST);
+        setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
         
         
         
         
-        
-        else
-        { System.out.print("Sorry, please just enter in 'Breakfast', 'Dinner', or 'Dessert'. \n");
-        }
-    
-    
-     System.out.println("Is there anything else you will like to add on your order? [y/n] ");
-     
-            /* choice = scan.next().charAt(0);
-             if (choice == 'y')
-             {
-             System.out.println("What whould you like? ");
-             input = scan.nextLine();
-             }
-             
     }
-     while(choice=='y');
-    */
+ 
     
-    
-    
-    
+    /** Returns an ImageIcon, or null if the path was invalid. */
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = Main.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+                return null;
+        }
     }
-    
+ 
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("CustomComboBoxDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        //Create and set up the content pane.
+        JComponent newContentPane = new Main();
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+ 
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+ 
+    class ComboBoxRenderer extends JLabel
+                           implements ListCellRenderer {
+        private Font uhOhFont;
+ 
+        public ComboBoxRenderer() {
+            setOpaque(true);
+            setHorizontalAlignment(CENTER);
+            setVerticalAlignment(CENTER);
+        }
+ 
+        /*
+         * This method finds the image and text corresponding
+         * to the selected value and returns the label, set up
+         * to display the text and image.
+         */
+        public Component getListCellRendererComponent(
+                                           JList list,
+                                           Object value,
+                                           int index,
+                                           boolean isSelected,
+                                           boolean cellHasFocus) {
+            //Get the selected index. (The index param isn't
+            //always valid, so just use the value.)
+            int selectedIndex = ((Integer)value).intValue();
+ 
+            if (isSelected) {
+                setBackground(list.getSelectionBackground());
+                setForeground(list.getSelectionForeground());
+            } else {
+                setBackground(list.getBackground());
+                setForeground(list.getForeground());
+            }
+ 
+            //Set the icon and text.  If icon was null, say so.
+            ImageIcon icon = images[selectedIndex];
+            String pet = petStrings[selectedIndex];
+            setIcon(icon);
+            if (icon != null) {
+                setText(pet);
+                setFont(list.getFont());
+            } else {
+                setUhOhText(pet + " (no image available)",
+                            list.getFont());
+            }
+ 
+            return this;
+        }
+ 
+        //Set the font and text when no image was found.
+        protected void setUhOhText(String uhOhText, Font normalFont) {
+            if (uhOhFont == null) { //lazily create this font
+                uhOhFont = normalFont.deriveFont(Font.ITALIC);
+            }
+            setFont(uhOhFont);
+            setText(uhOhText);
+        }
+    }
 }
